ce67918d9f6ec0ee34aae351487c87234a8afbc6
thenujahansidu <86600185+thenujahansidu@users.noreply.github.com>
2021-07-28 11:53:07 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,85 @@
+import random
+import time
+import os
+os.system('clear')
+def banner():
+    print('''
+\033[1;96m                                                                                                                                                                                             
+ _______   _______ ___________                        
+/  __ \ \ / | ___ |  ___| ___ \ 
+| /  \/\ V /| |_/ | |__ | |_/ /
+| |     \ / | ___ |  __||    /
+| \__/\ | | | |_/ | |___| |\ \ 
+ \____/ \_/ \____/\____/\_| \_|
+\033[1;93m'''.center(50))
+    time.sleep(1)
+    print(''' \033[1;93m
+______  ___  _____ _____ 
+| ___ \/ _ \/  ___/  ___|
+| |_/ / /_\ \ `--.\ `--.
+|  __/|  _  |`--. \`--. \ 
+| |   | | | /\__/ /\__/ /
+\_|   \_| |_\____/\____/ '''.center(50))
+    time.sleep(0.5)
+    print(''' \033[1;92m
+ _____ _____ _   _ ___________  ___ _____ ___________ 
+|  __ |  ___| \ | |  ___| ___ \/ _ |_   _|  _  | ___ \ 
+| |  \| |__ |  \| | |__ | |_/ / /_\ \| | | | | | |_/ /
+| | __|  __|| . ` |  __||    /|  _  || | | | | |    / 
+| |_\ | |___| |\  | |___| |\ \| | | || | \ \_/ | |\ \ 
+ \____\____/\_| \_\____/\_| \_\_| |_/\_/  \___/\_| \_|
+ '''.center(50))
+    time.sleep(0.5)
+banner()
+
+print("\033[03;91m")
+print("<<--CYBER THEJ-->>".center(50))
+time.sleep(0.5)
+print("\033[01;94m")
+print("<==SL CYBER WARRIORS==>".center(50))
+time.sleep(0.5)
+print("\033[01;92m")
+input("ENTER ANY KEY TO CONTINUE THIS TOOL :")
+print("\033[09;93;47m")
+print("SYSTEM STARTED!")
+print("LOADING........")
+time.sleep(0.2)
+print("LOADING........")
+time.sleep(0.3)
+print("LOADING........")
+time.sleep(0.4)
+print("LOADING........")
+time.sleep(0.5)
+print("LOADING........")
+time.sleep(0.6)
+print("LOADING........")
+time.sleep(0.7)
+print("LOADING........")
+time.sleep(0.8)
+print("LOADING........")
+time.sleep(0.9)
+print("LOADING........")
+time.sleep(1)
+print("LOADING........")
+time.sleep(1.1)
+print("LOADING........")
+time.sleep(1.2)
+print("LOADING........")
+time.sleep(1.3)
+print('\033[0m')
+
+print("")
+num = int(input("\033[1;34m ENTER A PASSWORD:\n"))
+print("")
+time.sleep(0.5)
+a = 1
+print("GENERATING!!!!........")
+print(".................")
+time.sleep(0.5)
+thej = random.randint(a,num)
+print("")
+print("Your password is : " + str(thej))
+print("")
+
+print("")
+print("<<--POWERED BY SL CYBER WARRIORS-->>".center(50))
